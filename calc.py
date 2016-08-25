cdf9e69a61e66037d6d8f875c9f6f831b511ef68
DaGal <dgalaktionov@enxenio.es>
2016-08-25 11:11:00 +0200

    Cursor navigation with indels

--- calc.py
+++ calc.py
@@ -4,6 +4,16 @@
 from math import *
 from sys import argv, stdin, stdout
 from tty import setraw, setcbreak
+from threading import Thread
+
+DEBUG=False
+
+class EscapeThread(Thread):
+	def __init__(self, *args, **kwargs):
+		Thread.__init__(self, daemon=True, *args, **kwargs)
+
+	def run(self):
+		stdin.read(1)
 
 def calculate(a):
 	try:
@@ -19,27 +29,60 @@ else:
 	stdout.write(">>> ")
 	stdout.flush()
 	a = ""
+	cursor = 0
 	while True:
 		c = stdin.read(1)[0]
 		co = ord(c)
-		if co in [3,4,27]:
+		if co in [3,4]: # CTRL+C, CTRL+D
 			break
+		elif co == 27: # ESC
+			# It could be an escape or the start of an arrow key
+			# Depends on the next bytes, if there are any
+			t = EscapeThread()
+			t.start()
+			t.join(0)
 
-		if co == 13:
+			if t.is_alive():
+				# If it's a real escape, exit
+				break
+		elif co == 68: # LEFT ARROW
+			cursor -= 1
+		elif co == 67: # RIGHT ARROW
+			cursor += 1
+		elif co == 13: # ENTER
 			stdout.write("\n\r")
 			calculate(a)
 			a = ""
-		elif co == 127:
-			a = a[:-1]
+			cursor = 0
+		elif co == 127: # BACKSPACE
+			if cursor > 0:
+				a = a[:cursor-1] + a[cursor:]
+				cursor -= 1
+		elif co == 126: # DEL
+			# In UNIX it's actually 2 byes that we have to remove
+			a = a[:cursor-1] + a[cursor+1:]
+			cursor -= 1
 		else:
-			a += c
+			a = a[:cursor] + c + a[cursor:]
+			cursor += 1
+
+		if cursor < 0:
+			cursor = 0
+		if cursor > len(a):
+			cursor = len(a)
 
 		stdout.write("\r")
-		stdout.write(" " * (len(a)+5))
+		stdout.write(" " * (len(a)+6))
 		stdout.write("\r")
 		stdout.write(">>> ")
-		stdout.write(a)
+		if DEBUG:
+			stdout.write(str(list(map(ord,a))))
+		else:
+			stdout.write(a)
+			stdout.write("\r")
+			stdout.write(">>> ")
+			stdout.write(a[:cursor])
 		stdout.flush()
 
-	stdout.write("\n\r")
+	stdout.write("\r\n")
 	stdout.flush()
